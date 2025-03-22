6b812a7eb3b33a0d24e5719128066ea41426c5f7
DuncanMathai <123893567+DuncanMathai@users.noreply.github.com>
2025-03-22 13:33:30 +0300

    Update Remove-Duplicates.py

--- Remove-Duplicates.py
+++ Remove-Duplicates.py
@@ -6,10 +6,9 @@ def removeDuplicates(nums):
         if nums[j] != nums[i]:
             i += 1
             nums[i] = nums[j]
-    return i + 1  # New length of array
+    return i + 1
 
-# Example usage:
 nums = [1, 1, 2]
 new_length = removeDuplicates(nums)
-print(new_length)  # Output: 2
-print(nums[:new_length])  # Output: [1, 2]
+print(new_length)  
+print(nums[:new_length])  
